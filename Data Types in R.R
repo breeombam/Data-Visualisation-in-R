fd2783ec57d3e73eda612f6ba731f7d4a054bd70
breeombam <86780798+breeombam@users.noreply.github.com>
2021-07-01 14:52:13 +0300

    Add files via upload

---
+++ Data Types in R.R
@@ -0,0 +1,76 @@
+# here, I was assigning values to variable names
+my_name<-'godfrey'
+myName<-'ombam'
+mas_201<-'sample surveys'
+mas_201
+mit_201<-"statistical computing"
+mit_201
+mma_215<-"calculus"
+mma_215
+mas_203<-"economic statistics"
+mas_203
+mas_202<-"vector analysis"
+class('sample survey')
+class('sample survey')
+my_number<-2
+class(my_number)
+my_teacher<-'3'
+class(my_teacher)
+'omom'->my_name
+'brenda'->my_name
+## factor
+## integers
+## logical
+## character
+##numeric
+class(my_name)
+
+###numeric data types
+class(0.5)
+#can be a decimal or a fraction
+class(3/4)
+my_number<-0.9
+my_number<-'0.9'
+print(my_number)
+class(my_number)
+print(my_number)
+class(my_number)
+
+##integers
+#is a whole number
+class(9)
+as.integer(0)
+as.integer(9)
+print(as.integer(0))
+as.integer(9)
+class(as.integer(90))
+integer_three<-(as.integer(7))
+brenda<-as.integer(9)
+class(brenda)
+class(as.integer(9))
+class(integer_three)
+
+##character
+class('omom')
+
+##logical
+#is true or false
+TRUE
+class(TRUE)
+FALSE
+class(FALSE)
+a<-TRUE
+class(a)
+print(a)
+class(a)
+##factor
+#used to refer to qualitative relationship
+c(1,2,3,4,5)
+class(c(1,2,3,4,5))
+c('bree',10,12)
+class(c('bree',10,12))
+c('blue','blue','black','black','red','red')
+class(c('blue','blue','black','black','red','red'))
+factorc('blue','blue','black','black','red','red')
+mathematics<-factor(c('blue','blue','black','black','red','red'))
+class(mathematics)
